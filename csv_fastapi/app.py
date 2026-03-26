14280f75b72d6e6198874617f326e4830502ffc4
ankitmrt123 <ankitsite1@gmail.com>
2026-03-26 14:42:58 +0530

    update message

--- csv_fastapi/app.py
+++ csv_fastapi/app.py
@@ -1,9 +1,30 @@
 from fastapi import FastAPI
+import pandas as pd
 
-app=FastAPI()
+app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"Message":"Welcome to FastAPI"}
+    return {"message": "Hello FastAPI"}
 
-# if "__name__"=="_main_":
+@app.get("/get-csv-data")
+def get_csv_data():
+
+    try:
+        df = pd.read_csv("students_complete.csv")
+
+        return {
+            "columns": list(df.columns),
+            "total_rows": len(df),
+            "data": df.fillna("").to_dict(orient="records")
+        }
+
+    except FileNotFoundError:
+        return {
+            "error": "students_complete.csv not found. Make sure it is in same folder as app.py"
+        }
+
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
